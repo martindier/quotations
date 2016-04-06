9b0ee27e3bcc5060fa829e351169755869dfc1a9
Martin Dier <martin.dier@gmail.com>
2016-04-06 21:12:43 +0100

    Signed-off-by: Martin Dier <martin.dier@gmail.com>£ tabs working q3 and copy deleted

--- quotations/www/js/app.js
+++ quotations/www/js/app.js
@@ -40,21 +40,21 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.home', {
+    url: '/home',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
+      'tab-home': {
+        templateUrl: 'templates/tab-home.html',
         controller: 'DashCtrl'
       }
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.test', {
+      url: '/test',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
+        'tab-test': {
+          templateUrl: 'templates/tab-test.html',
           controller: 'ChatsCtrl'
         }
       }
@@ -80,6 +80,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/home');
 
 });
