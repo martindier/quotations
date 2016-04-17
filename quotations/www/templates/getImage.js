ddcefa8566bc5baa1ad7ecaa7e17e2aa9cd4bda7
martindier <martin.dier@gmail.com>
2016-04-17 22:16:04 +0100

    uploaded and listed imagews
    Signed-off-by: martindier <martin.dier@gmail.com>

--- quotations/www/templates/getImage.js
+++ quotations/www/templates/getImage.js
@@ -16,3 +16,10 @@ var getPic=function() {
     }
   );
 }
+
+var image=['aurora.jpg','aurora2.jpg','aurora4.jpg','cherrryblossom.jpg','cherryb2.jpg','cherryb3.jpg','cherryb4.jpg','f7R1ZH3.jpg','GMythicalIreland8.jpg','ionic.png','kerrry.jpg','lake.jpg','lake2.jpg','maxresdefault.jpg','mike.png','mountainf.jpg','Mountains.jpg','MythicalIreland11.jpg','MythicalIreland12.jpg','MythicalIreland3.jpg','nlights2752392b.jpg','NorthernLig.jpg','northernlights.jpg','Northernlights31.jpg','northernlightslarge.jpg','NorthernLightsWinter.jpg','notheights.jpg','passagelintel.jpg','skelligislandsire.jpg','skelligs.jpg','sunrise.jpg','sunrise2.jpg','tara.jpg','trees.jpg','water.jpg'
+];
+// Random shuffle items
+image.sort(function() {return 0.5 - Math.random()})
+// Get third item in randomised array
+var itemImage = image[3]   
