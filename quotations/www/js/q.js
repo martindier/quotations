7c216d5a78e39d1dcbe49fc55963ff8bd17f59c3
martindier <martin.dier@gmail.com>
2016-04-18 12:36:08 +0100

    corrected syntax in quotations and set up buttons which I got working with alert functions....eventually....should be no spaces in alert message?? Downloaded gererds toast but it has no functionality?
    Signed-off-by: martindier <martin.dier@gmail.com>

--- quotations/www/js/q.js
+++ quotations/www/js/q.js
@@ -1,93 +1,86 @@
 var q= [
 ["lorum ipsum dolar", "Gerard", "funny"],
-["Light travels faster than sound. This is why some people appear bright until they speak","Steven Wright","sarcasm"];
-["Mirrors can’t talk, lucky for you they can’t laugh either","Annon","sacrasm"];
-["Violence won’t solve anything…But it sure makes me feel good","Annon","sarcasm"];
-["A child of five would understand this. Send someone to fetch a child of five","Groucho Marx","sarcasm"];
-["Age is not a particularly interesting subject. Anyone can get old. All you have to do is live long enough.","Groucho Marx","sarcasm"];
-["Either this man is dead or my watch has stopped","Groucho Marx","sarcasm"];
-["From the moment I picked up your book until I laid it down, I was convulsed with laughter. Some day I intend reading it.","Groucho Marx","sarcasm"];
-["I could dance with you until the cows come home. On second thought I'd rather dance with the cows until you come home","Groucho Marx","sarcasm"];
-["I find television very educating. Every time somebody turns on the set, I go into the other room and read a book","Groucho Marx","sarcasm"];
-["I never forget a face, but in your case I'll be glad to make an exception","Groucho Marx","sarcasm"];
-["I sent the club a wire stating, PLEASE ACCEPT MY RESIGNATION. I DON'T WANT TO BELONG TO ANY CLUB THAT WILL ACCEPT ME AS A MEMBER","Groucho Marx","sarcasm"];
-["I've had a perfectly wonderful evening. But this wasn't it","Groucho Marx","sarcasm"];
-["Military intelligence is a contradiction in terms","Groucho Marx","sarcasm"];
-["Military justice is to justice what military music is to music","Groucho Marx","sarcasm"];
-["Money frees you from doing things you dislike. Since I dislike doing nearly everything, money is handy","Groucho Marx","sarcasm"];
-["My mother loved children -- she would have given anything if I had been one","Groucho Marx","sarcasm"];
-["Outside of a dog, a book is man's best friend. Inside of a dog it's too dark to read","Groucho Marx","sarcasm"];
-["She got her looks from her father. He's a plastic surgeon","Groucho Marx","sarcasm"];
-["There's one way to find out if a man is honest-ask him. If he says, "Yes," you know he is a crook","Groucho Marx","sarcasm"];
-["Those are my principles, and if you don't like them....... well, I have others","Groucho Marx","sarcasm"];
-["Time flies like an arrow. Fruit flies like a banana","Groucho Marx","sarcasm"];
-["Last words are for fools who haven't said enough","Groucho Marx","sarcasm"];];
-["Reason has always existed, but not always in a reasonable form","Karl Marx","sarcasm"];
-["The last capitalist we hang shall be the one who sold us the rope.","Karl Marx","sarcasm"];
-["Religion is the sigh of the oppressed creature, the heart of a heartless world, just as it is the spirit of a spiritless situation. It is the opium of the people","Karl Marx","reflective"];
-["The philosophers have only interpreted the world, in various ways. The point, however, is to change it","Karl Marx","reflective"];
-["Do not worry about your difficulties in Mathematics. I can assure you mine are still greater","Albert Einstein","positive"];
-["Gravitation cannot be held responsible for people falling in love. How on earth can you explain in terms of chemistry and physics so important a biological phenomenon as first love? Put your hand on a stove for a minute and it seems like an hour. Sit with that special girl for an hour and it seems like a minute. That's relativity.","Albert Einstein","positive"];
-["I know not with what weapons World War III will be fought, but World War IV will be fought with sticks and stones","Albert Einstein","reflective"];
-["I never teach my pupils. I only attempt to provide the conditions in which they can learn","Albert Einstein","reflective"];
-["I never think of the future - it comes soon enough","Albert Einstein","reflective"];
-["If I had only known, I would have been a locksmith","Albert Einstein","sarcastic"];
-["If you are out to describe the truth, leave elegance to the tailor","Albert Einstein","happy"];
-["Imagination is more important than knowledge","Albert Einstein","positive"];
-["It is a miracle that curiosity survives formal education","Albert Einstein","reflective"];
-["It is the supreme art of the teacher to awaken joy in creative expression and knowledge","Albert Einstein","reflective"];
-["Only two things are infinite, the universe and human stupidity, and I'm not sure about the former","Albert Einstein","happy"];
-["The most beautiful thing we can experience is the mysterious. It is the source of all true art and science.","Albert Einstein","positive"];
-["Anger as soon as fed is dead - 'Tis starving makes it fat","Emily Dickinson","reflective"];
-["Forever is composed of nows","Emily Dickinson","reflective"];
-["Hope is the thing with feathers that perches in the soul","Emily Dickinson","reflective"];
-["I dwell in possibility...","Emily Dickinson","positive"];
-["One need not be a chamber to be haunted, One need not be a house, The brain has corridors surpassing Material place","Emily Dickinson","reflective"];
-["Success is counted sweetest by those who ne'er succeed","Emily Dickinson","reflective"];
-["They say that God is everywhere, and yet we always think of Him as somewhat of a recluse","Emily Dickinson","happy"];
-["We turn not older with years, but newer every day","Emily Dickinson","happy"];
-["A little Madness in the Spring Is wholesome even for the King","Emily Dickinson","reflective"];
+["Light travels faster than sound. This is why some people appear bright until they speak","Steven Wright","sarcasm"],
+["Mirrors can’t talk, lucky for you they can’t laugh either","Annon","sacrasm"],
+["Violence won’t solve anything…But it sure makes me feel good","Annon","sarcasm"],
+["A child of five would understand this. Send someone to fetch a child of five","Groucho Marx","sarcasm"],
+["Age is not a particularly interesting subject. Anyone can get old. All you have to do is live long enough.","Groucho Marx","sarcasm"],
+["Either this man is dead or my watch has stopped","Groucho Marx","sarcasm"],
+["From the moment I picked up your book until I laid it down, I was convulsed with laughter. Some day I intend reading it.","Groucho Marx","sarcasm"],
+["I could dance with you until the cows come home. On second thought I'd rather dance with the cows until you come home","Groucho Marx","sarcasm"],
+["I find television very educating. Every time somebody turns on the set, I go into the other room and read a book","Groucho Marx","sarcasm"],
+["I never forget a face, but in your case I'll be glad to make an exception","Groucho Marx","sarcasm"],
+["I sent the club a wire stating, PLEASE ACCEPT MY RESIGNATION. I DON'T WANT TO BELONG TO ANY CLUB THAT WILL ACCEPT ME AS A MEMBER","Groucho Marx","sarcasm"],
+["I've had a perfectly wonderful evening. But this wasn't it","Groucho Marx","sarcasm"],
+["Military intelligence is a contradiction in terms","Groucho Marx","sarcasm"],
+["Military justice is to justice what military music is to music","Groucho Marx","sarcasm"],
+["Money frees you from doing things you dislike. Since I dislike doing nearly everything, money is handy","Groucho Marx","sarcasm"],
+["My mother loved children -- she would have given anything if I had been one","Groucho Marx","sarcasm"],
+["Outside of a dog, a book is man's best friend. Inside of a dog it's too dark to read","Groucho Marx","sarcasm"],
+["She got her looks from her father. He's a plastic surgeon","Groucho Marx","sarcasm"],
+["There's one way to find out if a man is honest-ask him. If he says - Yes, you know he is a crook","Groucho Marx","sarcasm"],
+["Those are my principles, and if you don't like them....... well, I have others","Groucho Marx","sarcasm"],
+["Time flies like an arrow. Fruit flies like a banana","Groucho Marx","sarcasm"],
+["Last words are for fools who haven't said enough","Groucho Marx","sarcasm"],
+["Reason has always existed, but not always in a reasonable form","Karl Marx","sarcasm"],
+["The last capitalist we hang shall be the one who sold us the rope.","Karl Marx","sarcasm"],
+["Religion is the sigh of the oppressed creature, the heart of a heartless world, just as it is the spirit of a spiritless situation. It is the opium of the people","Karl Marx","reflective"],
+["The philosophers have only interpreted the world, in various ways. The point, however, is to change it","Karl Marx","reflective"],
+["Do not worry about your difficulties in Mathematics. I can assure you mine are still greater","Albert Einstein","positive"],
+["Gravitation cannot be held responsible for people falling in love. How on earth can you explain in terms of chemistry and physics so important a biological phenomenon as first love? Put your hand on a stove for a minute and it seems like an hour. Sit with that special girl for an hour and it seems like a minute. That's relativity.","Albert Einstein","positive"],
+["I know not with what weapons World War III will be fought, but World War IV will be fought with sticks and stones","Albert Einstein","reflective"],
+["I never teach my pupils. I only attempt to provide the conditions in which they can learn","Albert Einstein","reflective"],
+["I never think of the future - it comes soon enough","Albert Einstein","reflective"],
+["If I had only known, I would have been a locksmith","Albert Einstein","sarcastic"],
+["If you are out to describe the truth, leave elegance to the tailor","Albert Einstein","happy"],
+["Imagination is more important than knowledge","Albert Einstein","positive"],
+["It is a miracle that curiosity survives formal education","Albert Einstein","reflective"],
+["It is the supreme art of the teacher to awaken joy in creative expression and knowledge","Albert Einstein","reflective"],
+["Only two things are infinite, the universe and human stupidity, and I'm not sure about the former","Albert Einstein","happy"],
+["The most beautiful thing we can experience is the mysterious. It is the source of all true art and science.","Albert Einstein","positive"],
+["Anger as soon as fed is dead - 'Tis starving makes it fat","Emily Dickinson","reflective"],
+["Forever is composed of nows","Emily Dickinson","reflective"],
+["Hope is the thing with feathers that perches in the soul","Emily Dickinson","reflective"],
+["I dwell in possibility...","Emily Dickinson","positive"],
+["One need not be a chamber to be haunted, One need not be a house, The brain has corridors surpassing Material place","Emily Dickinson","reflective"],
+["Success is counted sweetest by those who ne'er succeed","Emily Dickinson","reflective"],
+["They say that God is everywhere, and yet we always think of Him as somewhat of a recluse","Emily Dickinson","happy"],
+["We turn not older with years, but newer every day","Emily Dickinson","happy"],
+["A little Madness in the Spring Is wholesome even for the King","Emily Dickinson","reflective"],
 ["Carpe Diem - sieze the day","Annon","positive"];
-["It is our light, not our darkness that most frightens us. We ask ourselves, 'Who am I to be brilliant, gorgeous, talented, fabulous?' Actually, who are you not to be? You are a child of God. Your playing small does not serve the world. There is nothing enlightened about shrinking so that other people won't feel insecure around you. We are all meant to shine","Marianne Wiliamson","positive"];
-["as we let our own light shine, we unconsciously give other people permission to do the same. As we are liberated from our own fear, our presence automatically liberates others.","Marianne Wiliamson","positive"];
+["It is our light, not our darkness that most frightens us. We ask ourselves, 'Who am I to be brilliant, gorgeous, talented, fabulous?' Actually, who are you not to be? You are a child of God. Your playing small does not serve the world. There is nothing enlightened about shrinking so that other people won't feel insecure around you. We are all meant to shine","Marianne Wiliamson","positive"],
+["as we let our own light shine, we unconsciously give other people permission to do the same. As we are liberated from our own fear, our presence automatically liberates others.","Marianne Wiliamson","positive"],
 ["The snow goose need do nothing but be itself","Annon","reflective"];
-["Everything we do is infused with the energy with which we do it. If we're frantic, life will be frantic. If we're peaceful, life will be peaceful. And so our goal in any situation becomes inner peace.","Marianne Wiliamson","positive"];
+["Everything we do is infused with the energy with which we do it. If we're frantic, life will be frantic. If we're peaceful, life will be peaceful. And so our goal in any situation becomes inner peace.","Marianne Wiliamson","positive"],
 ["In every community, there is work to be done. In every nation, there are wounds to heal. In every heart, there is the power to do it","Marianne Wiliamson","positive"];
-["Just like a sunbeam can't separate itself from the sun, and a wave can't separate itself from the ocean, we can't separate ourselves from one another. We are all part of a vast sea of love, one indivisible divine mind","Marianne Wiliamson","positive"];
-["Our prime purpose in this life is to help others. And if you can’t help them, at least don’t hurt them."
-["If you think you are too small to make a difference, try sleeping with a mosquito ","Dalai Lama","positive"];
-["My religion is very simple. My religion is kindness.","Dalai Lama","positive"];
-["Nothing is more sad than the death of an illusion","Koestler","reflective"];
-["We do evil in the hopes something good comes out of it","Annon","reflective"];
-["Light reaches a place to find out darkness already waiting for it there...","Annon","sad"];
-["There are more dead people than living. And their numbers are increasing. The living are getting rarer.","Eugene Ionesco","sad"];
-["It's not that I'm afraid to die, I just don't want to be there when it happens.","Woody Allen","sad"];
-["A man dies ... only a few circles in the water prove that he was ever there. And even they quickly disappear. And when they're gone, he's forgotten, without a trace, as if he'd never even existed. And that's all.","Wolfgang Borchert","sad"];
-["Cowards die many times before their deaths, The valiant never taste of death but once.","William Shakespeare","reflective"];
-["Better to flee from death than feel its grip","Homer","reflective"];
-["You only live twice. Once when you are born and once when you look death in the face.","Ian Flemming","reflective"];
-["The grave itself is but a covered bridge, Leading from light to light, through a brief darkness!","Henry Wadsworth Longfellow"];
-["As soon as one is born, one starts dying","Luigi Pirandello","sad"
-["Brief and powerless is man's life; on him and all his race the slow, sure doom falls pitiless and dark","Bertrand Russel","sad"];
-["Death is when the monsters get you","Stephen King","sad"];
-["A couch of thorns, or an embroidered bed, Are matters of indifference to the dead","Theogins of Megegara","sad"];
-["Death aims only once, but never misses","Edward Counsel","sad"];
-["For death is but a passing phase of Life;
-A change of dress, a disrobing;
-A birth into the unborn again;
-A commencing where we ended;
-A starting where we stopped to rest;
-A crossroad of Eternity;
-A giving up of something, to possess all things.
-The end of the unreal, the beginning of the real
-", "Edwin Leibfreed", "reflective"];
+["Just like a sunbeam can't separate itself from the sun, and a wave can't separate itself from the ocean, we can't separate ourselves from one another. We are all part of a vast sea of love, one indivisible divine mind","Marianne Wiliamson","positive"],
+["Our prime purpose in this life is to help others. And if you can’t help them, at least don’t hurt them.","Dalai Lama","positive"]
+["If you think you are too small to make a difference, try sleeping with a mosquito ","Dalai Lama","positive"],
+["My religion is very simple. My religion is kindness.","Dalai Lama","positive"],
+["Nothing is more sad than the death of an illusion","Koestler","reflective"],
+["We do evil in the hopes something good comes out of it","Annon","reflective"],
+["Light reaches a place to find out darkness already waiting for it there...","Annon","sad"],
+["There are more dead people than living. And their numbers are increasing. The living are getting rarer.","Eugene Ionesco","sad"],
+["It's not that I'm afraid to die, I just don't want to be there when it happens.","Woody Allen","sad"],
+["A man dies ... only a few circles in the water prove that he was ever there. And even they quickly disappear. And when they're gone, he's forgotten, without a trace, as if he'd never even existed. And that's all.","Wolfgang Borchert","sad"],
+["Cowards die many times before their deaths, The valiant never taste of death but once.","William Shakespeare","reflective"],
+["Better to flee from death than feel its grip","Homer","reflective"],
+["You only live twice. Once when you are born and once when you look death in the face.","Ian Flemming","reflective"],
+["The grave itself is but a covered bridge, Leading from light to light, through a brief darkness!","Henry Wadsworth Longfellow"],
+["As soon as one is born, one starts dying","Luigi Pirandello","sad"],
+["Brief and powerless is man's life; on him and all his race the slow, sure doom falls pitiless and dark","Bertrand Russel","sad"],
+["Death is when the monsters get you","Stephen King","sad"],
+["A couch of thorns, or an embroidered bed, Are matters of indifference to the dead","Theogins of Megegara","sad"],
+["Death aims only once, but never misses","Edward Counsel","sad"],
+["For death is but a passing phase of Life, A change of dress, a disrobing; A birth into the unborn again; A commencing where we ended; A starting where we stopped to rest; A crossroad of Eternity; A giving up of something, to possess all things.The end of the unreal, the beginning of the real", "Edwin Leibfreed", "reflective"]
 ];
 
 // Random shuffle items
 q.sort(function() {return 0.5 - Math.random()})
 // Get third item in randomised array
-var item = x[3]   
+var item = x[2]   
 console.log (q.length);
     console.log(q[2]);
+    console.log("item");
       alert(q.length);
-      alert(q[3])
+      alert(q[2])
