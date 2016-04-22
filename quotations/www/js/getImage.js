d2efca57bdf52927cf75e91026c5ffe8946c0ef1
martindier <martin.dier@gmail.com>
2016-04-22 20:47:16 +0100

    basic image functionality works from gallery
    Signed-off-by: martindier <martin.dier@gmail.com>

--- quotations/www/js/getImage.js
+++ quotations/www/js/getImage.js
@@ -1,3 +1,19 @@
+// To allow a random picture to be chosen
+var random_images_array=['aurora.jpg','aurora2.jpg','aurora4.jpg','cherrryblossom.jpg','cherryb2.jpg','cherryb3.jpg','cherryb4.jpg','f7R1ZH3.jpg','GMythicalIreland8.jpg','kerrry.jpg','lake.jpg','lake2.jpg','maxresdefault.jpg','mountainf.jpg','Mountains.jpg','MythicalIreland11.jpg','MythicalIreland12.jpg','MythicalIreland3.jpg','nlights2752392b.jpg','NorthernLig.jpg','northernlights.jpg','Northernlights31.jpg','northernlightslarge.jpg','NorthernLightsWinter.jpg','notheights.jpg','passagelintel.jpg','skelligislandsire.jpg','skelligs.jpg','sunrise.jpg','sunrise2.jpg','tara.jpg','trees.jpg','water.jpg'
+];
+function getRandomImage(imgAr, path) {
+    path = path || 'img/'; 
+    var num = Math.floor( Math.random() * random_images_array.length );
+    var img = random_images_array[ num ];
+    var imgStr = '<img src="' + path + img + '" alt = "" width="305" height="312" id="myPicture"> ';
+    document.write(imgStr); document.close();
+}
+
+
+
+
+
+
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     console.log(navigator.camera);
@@ -24,10 +40,4 @@ function getImage(){getPictures(
 
 
 
-// To allow a random picture to be chosen
-var image=['aurora.jpg','aurora2.jpg','aurora4.jpg','cherrryblossom.jpg','cherryb2.jpg','cherryb3.jpg','cherryb4.jpg','f7R1ZH3.jpg','GMythicalIreland8.jpg','ionic.png','kerrry.jpg','lake.jpg','lake2.jpg','maxresdefault.jpg','mike.png','mountainf.jpg','Mountains.jpg','MythicalIreland11.jpg','MythicalIreland12.jpg','MythicalIreland3.jpg','nlights2752392b.jpg','NorthernLig.jpg','northernlights.jpg','Northernlights31.jpg','northernlightslarge.jpg','NorthernLightsWinter.jpg','notheights.jpg','passagelintel.jpg','skelligislandsire.jpg','skelligs.jpg','sunrise.jpg','sunrise2.jpg','tara.jpg','trees.jpg','water.jpg'
-];
-// Random shuffle items
-image.sort(function() {return 0.5 - Math.random()})
-// Get third item in randomised array
-var itemImage = image[2]   
+
